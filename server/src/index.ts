41315fd0c88879ab58af69226aa5db9059b6a729
Cole Flournoy <flournoy.cole@gmail.com>
2025-03-21 22:18:36 -0400

    Specify cors for internal api requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+const cors = require('cors');
+const corsOptions = {
+   origin: 'http://localhost:5173',
+}
+
+app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
