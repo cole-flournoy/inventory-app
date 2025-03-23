5189e6d18cad0fdcc4f7595325fdc97de1c4ec69
Cole Flournoy <flournoy.cole@gmail.com>
2025-03-23 14:58:59 -0400

    Add sign up page and form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/login'
+import Login from './pages/Login'
 import InventoryTable from './pages/InventoryTable'
 import ItemDetail from './pages/ItemDetail'
+import SignUp from './pages/SignUp'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>        
         <Route path="/" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="/inventoryTable" element={<InventoryTable />} />
         <Route path="/itemDetail" element={<ItemDetail />} />
       </Routes>
---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,102 @@
+import Visibility from "@mui/icons-material/Visibility"
+import VisibilityOff from "@mui/icons-material/VisibilityOff"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import FormControl from "@mui/material/FormControl"
+import IconButton from "@mui/material/IconButton"
+import InputAdornment from "@mui/material/InputAdornment"
+import InputLabel from "@mui/material/InputLabel"
+import OutlinedInput from "@mui/material/OutlinedInput"
+import TextField from "@mui/material/TextField"
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+
+export default function SignUp() {
+   const [showPassword, setShowPassword] = useState(false)
+   const [signUpFormState, setSignUpFormState] = useState({ 
+      name: '',
+      company: '',
+      username: '', 
+      password: '', 
+   })
+   const navigate = useNavigate()
+   
+   const handleSubmitSignUp = () => {
+      console.log(signUpFormState)
+   }
+
+   return (
+      <div style={{ 
+         display: 'flex', 
+         justifyContent: 'center', 
+         alignItems: 'center', 
+         height: '100vh', 
+         width: '100vw',
+      }}>
+         <Box sx={{ 
+            border: '1px solid lightgrey', 
+            padding: '20px', 
+            borderRadius: '4px', 
+            display: 'flex',
+            flexDirection: 'column',
+            backgroundColor: 'white',
+            rowGap: '15px',
+            width: '300px',
+         }}>
+            <h2>Sign Up</h2>
+            <TextField
+               id="name"
+               label="Name"
+               variant="outlined"
+               value={signUpFormState.name}
+               onChange={(e) => setSignUpFormState({ ...signUpFormState, name: e.target.value })}
+            />
+            <TextField
+               id="company"
+               label="Company"
+               variant="outlined"
+               value={signUpFormState.company}
+               onChange={(e) => setSignUpFormState({ ...signUpFormState, company: e.target.value })}
+            />
+            <TextField 
+               id="username" 
+               label="Username" 
+               variant="outlined" 
+               value={signUpFormState.username}
+               onChange={(e) => setSignUpFormState({ ...signUpFormState, username: e.target.value })}
+            />
+            <FormControl sx={{ }} variant="outlined">
+               <InputLabel htmlFor="password">Password</InputLabel>
+               <OutlinedInput
+                  id="password"
+                  type={showPassword ? 'text' : 'password'}
+                  value={signUpFormState.password}
+                  onChange={(e) => setSignUpFormState({ ...signUpFormState, password: e.target.value })}
+                  endAdornment={
+                  <InputAdornment position="end">
+                     <IconButton
+                        aria-label={ showPassword ? 'hide the password' : 'display the password' }
+                        onClick={() => setShowPassword((show) => !show)}
+                        edge="end"
+                     >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                     </IconButton>
+                  </InputAdornment>
+                  }
+                  label="Password"
+               />
+            </FormControl>
+            <Button 
+               variant="contained" 
+               sx={{ backgroundColor: "#692f6b" }}
+               onClick={handleSubmitSignUp}
+            >Sign Up</Button>
+            <Button 
+               variant="outlined" 
+               sx={{ color: "#692f6b", borderColor: "#692f6b" }}
+               onClick={() => navigate('/')}
+            >Log In</Button>
+         </Box>
+      </div>
+   )
+}
