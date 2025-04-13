3e52b8558ab1705f0b457533e722db03669327b5
Cole Flournoy <flournoy.cole@gmail.com>
2025-04-12 20:30:01 -0400

    Add toasts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,22 @@ import Login from './pages/Login'
 import InventoryTable from './pages/InventoryTable'
 import ItemDetail from './pages/ItemDetail'
 import SignUp from './pages/SignUp'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
-    <Router>
-      <Routes>        
-        <Route path="/" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/inventoryTable" element={<InventoryTable />} />
-        <Route path="/itemDetail" element={<ItemDetail />} />
-      </Routes>
-    </Router>
+    <>
+      <Toaster />
+      <Router>
+        <Routes>        
+          <Route path="/" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/inventoryTable" element={<InventoryTable />} />
+          <Route path="/itemDetail" element={<ItemDetail />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
